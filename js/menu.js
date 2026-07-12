27fa4147ef05908e9b42eb0f0c11d4b42def7ea4
unknown <macarriazo@gmail.com>
2026-07-12 16:46:44 +0200

    tools

--- js/menu.js
+++ js/menu.js
@@ -99,7 +99,7 @@ document.addEventListener("DOMContentLoaded", function() {
     function toolsItem() {
         return `
             <li class="has-sub">
-                <a href="${basePath}tools/cisa-kev/index.html" class="nav-link${A('tools')}">
+                <a href="${basePath}tools/index.html" class="nav-link${A('tools')}">
                     <svg viewBox="0 0 24 24"><path d="M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z"/></svg>
                     Tools
                     <svg class="chevron" viewBox="0 0 24 24"><polyline points="6 9 12 15 18 9"/></svg>
